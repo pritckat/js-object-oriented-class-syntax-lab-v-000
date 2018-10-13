cb827b4a2b1b29f8117bb8a4b12517dcd0a1cf60
Katrina Pritchard <pritckat@gmail.com>
2018-10-13 19:21:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ class BoardMember {
     this.homeState = homeState
     this.training = training
   }
+
+  veto() {
+    return "No, I must disagree"
+  }
+
+  approve() {
+    return "You can do that!"
+  }
+
+  doCharity() {
+    return "I like to help people."
+  }
+
+  releasePressStatement() {
+    return "You will see great things from Scuber."
+  }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
 }
